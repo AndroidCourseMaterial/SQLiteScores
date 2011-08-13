489f58bfc2f98b3114c6f3c4393a98d01296caab
David Fisher <fisherds@rose-hulman.edu>
2011-08-12 23:24:31 -0700

    Cleaning up the SQLite starting point ListActivity javadoc

--- src/edu/rosehulman/sqlhighscores/ScoresListActivity.java
+++ src/edu/rosehulman/sqlhighscores/ScoresListActivity.java
@@ -3,9 +3,10 @@ package edu.rosehulman.sqlhighscores;
 import java.util.ArrayList;
 import java.util.Collections;
 
-import android.app.Activity;
 import android.app.Dialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Menu;
@@ -13,37 +14,52 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Toast;
 
 /**
  * Activity that displays a list view of Names & Scores
+ * Currently using no data storage
+ * Need to add SQLite for data storage
  * 
- * @author fisherds
+ * @author Dave Fisher
  *
  */
-public class ScoresListActivity extends Activity {
+public class ScoresListActivity extends ListActivity {
 
 	/**
-	 * Dialog for adding and editing scores
+	 * TAG for debug log messages
+	 */
+	public static final String TAG = "Scores";
+	
+	/**
+	 * Dialog ID for adding and editing scores (one dialog for both tasks)
 	 */
 	private static final int DIALOG_ID = 1;
 
 	/**
-	 * The list view that fills the entire screen 
+	 * Constant to indicate that no row is selected for editing
+	 * Used when adding a new score entry
 	 */
-	private ListView mScoresListView;
-	
-	public static final long NEW_ENTRY = -1;
-	private long mEditingScoreId = NEW_ENTRY;
+	public static final long NO_ID_SELECTED = -1;
 
+	/**
+	 * Index of the score / row selected
+	 */
+	private long mSelectedId = NO_ID_SELECTED;
+
+	/**
+	 * Array holding the scores
+	 */
 	private ArrayList<Score> mScores = new ArrayList<Score>();
+
+	/**
+	 * Adapter to fill the List View with mScores data
+	 * Note: Could use the ListActivity function getListAdapter, but that results in a lot of ugly casting
+	 */
 	private ArrayAdapter<Score> mScoreAdapter;
 
 	/** Called when the activity is first created. */
@@ -51,50 +67,66 @@ public class ScoresListActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.scores_list_activity);
-
-		mScoresListView = (ListView) findViewById(R.id.scores_list_view);
-		registerForContextMenu(mScoresListView);
-
-		mScoreAdapter = new ArrayAdapter<Score>(this, android.R.layout.simple_list_item_1, mScores);
-		mScoresListView.setAdapter(mScoreAdapter);
-		mScoresListView.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				launchEditDialog(id);
-			}});
+		int viewResourceId = android.R.layout.simple_list_item_1; // Using a built in view for now
+		mScoreAdapter = new ArrayAdapter<Score>(this, viewResourceId, mScores);
+		setListAdapter(mScoreAdapter);  // Magic function for a ListActivity to set the adapter
+		registerForContextMenu(getListView());
 	}
 
-	protected void launchEditDialog(long id) {
-		mEditingScoreId = id;
+	/**
+	 * ListActivity sets up the onItemClick listener for the list view automatically via this function
+	 */
+	@Override
+	protected void onListItemClick(ListView listView, View selectedView, int position, long id) {
+		super.onListItemClick(listView, selectedView, position, id);
+		mSelectedId = id;
 		showDialog(DIALOG_ID);
 	}
 
+	/**
+	 * Standard menu.  Only has one item
+	 * CONSIDER: Could add an edit and/or remove option when an item is selected
+	 */
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
 		MenuInflater inflater = getMenuInflater();
-		inflater.inflate(R.menu.main_menu, menu);
+		inflater.inflate(R.menu.options_menu, menu);
 		return true;
 	}
 
+	/**
+	 * Standard listener for the option menu item selections
+	 */
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		super.onOptionsItemSelected(item);
-		if (item.getItemId() == R.id.menu_add) {
-			mEditingScoreId = NEW_ENTRY;
+		switch(item.getItemId()) {
+		case R.id.menu_add:
+			mSelectedId = NO_ID_SELECTED;
 			showDialog(DIALOG_ID);
+			return true;
+		default:
+			return false;
 		}
-		return false;
 	}
-
+	
+	/**
+	 * Create a context menu for the list view
+	 * Secretly surprised ListActivity doesn't provide a special magic feature here too. :)
+	 */
 	@Override
 	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
 		super.onCreateContextMenu(menu, v, menuInfo);
 		MenuInflater inflator = getMenuInflater();
-		if(v == mScoresListView) {
+		if(v == getListView()) {
 			inflator.inflate(R.menu.scores_list_view_context_menu, menu);
 		}
 	}
 
+	/**
+	 * Standard listener for the context menu item selections
+	 */
 	@Override
 	public boolean onContextItemSelected(MenuItem item) {
 		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
@@ -103,45 +135,87 @@ public class ScoresListActivity extends Activity {
 			removeScore(info.id);
 			return true;
 		case R.id.menu_item_list_view_edit:
-			launchEditDialog(info.id);
+			mSelectedId = info.id;
+			showDialog(DIALOG_ID);
 			return true;
 		}
 		return super.onContextItemSelected(item);
 	}
 
+	/**
+	 * Called when the activity is removed from memory (placeholder for later)
+	 */
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
 	}
+	
 
+    // ======================================================================
+    // Data CRUD mechanisms (Create, read, update, and delete)
+    // ======================================================================
+
+	/**
+	 * Create: Add a new score to the data storage mechanism
+	 * @param s New score to add
+	 */
 	private void addScore(Score s) {
 		mScores.add(s);
 		Collections.sort(mScores);
 		mScoreAdapter.notifyDataSetChanged();
 	}
 
+	/**
+	 * Read: Get a score for the data storage mechanism
+	 * @param id Index of the score in the data storage mechanism 
+	 */
+	private Score getScore(long id) {
+		return mScores.get((int) id);
+	}
+	
+	/**
+	 * Update: Edit a score in the data storage mechanism
+	 * Uses the values in the pass Score to updates the score at the mSelectedId location
+	 * @param s Container for the new values to use in the update
+	 */
+	private void editScore(Score s) {
+		if (mSelectedId == NO_ID_SELECTED) {
+			Log.e(TAG, "Attempt to update with no score selected.");
+		}
+		Score selectedScore = getScore(mSelectedId);
+		selectedScore.setName(s.getName());
+		selectedScore.setScore(s.getScore());
+		Collections.sort(mScores);
+		mScoreAdapter.notifyDataSetChanged();
+	}
+
+	/**
+	 * Delete: Remove a score from the data storage mechanism
+	 * @param id Index of the score in the data storage mechanism
+	 */
 	private void removeScore(long id) {
 		mScores.remove((int)id);
 		Collections.sort(mScores);
 		mScoreAdapter.notifyDataSetChanged();
 	}
+	
 
-	private void editScore(Score s) {
-		mScores.get((int) mEditingScoreId).setName(s.getName());
-		mScores.get((int) mEditingScoreId).setScore(s.getScore());
-		Collections.sort(mScores);
-		mScoreAdapter.notifyDataSetChanged();
-	}
-
+    // ======================================================================
+    // Dialog for adding and updating Scores
+    // ======================================================================
+	
+	/**
+	 * Create the dialog if it has never been launched
+	 * Uses a custom dialog layout
+	 */
 	@Override
 	protected Dialog onCreateDialog(int id) {
 		super.onCreateDialog(id);
 		final Dialog dialog = new Dialog(this);
 		switch (id) {
 		case DIALOG_ID:
-			dialog.setContentView(R.layout.add_dialog);
+			dialog.setContentView(R.layout.score_dialog);
 			dialog.setTitle(R.string.add_score);
-
 			final EditText nameText = (EditText) dialog.findViewById(R.id.name_entry);
 			final EditText scoreText = (EditText) dialog.findViewById(R.id.score_entry);
 			final Button confirmButton = (Button) dialog.findViewById(R.id.confirm_score_button);
@@ -150,14 +224,14 @@ public class ScoresListActivity extends Activity {
 			confirmButton.setOnClickListener(new OnClickListener() {
 				@Override
 				public void onClick(View v) {
-					Score s = new Score();
+					Score s = new Score();  // Create an object to hold the values
 					s.setName(nameText.getText().toString());
 					try {
 						s.setScore(Integer.parseInt(scoreText.getText().toString()));
 					} catch (NumberFormatException e) {
 						s.setScore(0);
 					}
-					if (mEditingScoreId == NEW_ENTRY) {
+					if (mSelectedId == NO_ID_SELECTED) {
 						addScore(s);	
 					} else {
 						editScore(s);
@@ -179,27 +253,29 @@ public class ScoresListActivity extends Activity {
 		return dialog;
 	}
 
+	/**
+	 * Update the dialog with appropriate text before presenting to the user
+	 */
 	@Override
 	protected void onPrepareDialog(int id, Dialog dialog) {
 		super.onPrepareDialog(id, dialog);
 		switch (id) {
 		case DIALOG_ID:
-
 			final EditText nameText = (EditText) dialog.findViewById(R.id.name_entry);
 			final EditText scoreText = (EditText) dialog.findViewById(R.id.score_entry);
 			final Button confirmButton = (Button) dialog.findViewById(R.id.confirm_score_button);
-			
-			if (mEditingScoreId == NEW_ENTRY) {
+			if (mSelectedId == NO_ID_SELECTED) {
+				dialog.setTitle(R.string.add_score);
 				confirmButton.setText(R.string.add);
 				nameText.setText("");
 				scoreText.setText("");
 			} else {
+				dialog.setTitle(R.string.update_score);
 				confirmButton.setText(R.string.update);
-				nameText.setText(mScores.get((int) mEditingScoreId).getName());
-				scoreText.setText("" + mScores.get((int) mEditingScoreId).getScore());
+				nameText.setText(mScores.get((int) mSelectedId).getName());
+				scoreText.setText("" + mScores.get((int) mSelectedId).getScore());
 			}
 			break;
-
 		}
 	}
 }
